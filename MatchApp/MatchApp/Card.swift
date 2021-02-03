8ed13630b45f94f176f05d1fd61c804b102b4726
Paulstar200 <vitalispaul48@live.com>
2021-02-03 10:29:46 +0300

    Add Card Model

---
+++ MatchApp/MatchApp/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  MatchApp
+//
+//  Created by MacBook Pro on 03/02/2021.
+//
+
+import Foundation
+
+class Card {
+    var imageName = ""
+    var isMatched = false
+    var isFlipped = false
+}
